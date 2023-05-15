0dd1fe334bdacf003b500fa39876e4097bbc36db
G Narsimha Reddy <98202100+Narsimha1202@users.noreply.github.com>
2023-05-15 12:55:16 +0530

    Add files via upload

---
+++ contracts/learn/DataTypes.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract DataType{
+
+    uint8 num8 = 10;
+    uint16 num16 = 1234;
+    uint256 num256 = 121212121;
+    uint num = 12122334436546567;
+
+
+    uint[] numArray = [1234, 4567,5678,7890];
+    
+     
+    int NegPos = -12;
+    int256 NegPos256 = -234123;
+
+
+
+    bool constant TorF = false;
+
+}
---
+++ contracts/learn/StringandArray.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+contract StringArray{
+    string name = "Mittu";
+    string [] public arr = ["apple", "mango", "banana"];
+    
+}
